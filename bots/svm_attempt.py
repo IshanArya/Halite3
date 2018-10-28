92d12cc607ba6ef091f92fcfec2944515e6150b8
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2018-10-28 13:05:43 -0400

    beginning attempt for svm bot

---
+++ bots/svm_attempt.py
@@ -0,0 +1,6 @@
+import sklearn
+import numpy
+import parse
+import os
+print(os.getcwd())
+parse.parse_replay_file('/Users/bharddwajvemulapalli/Documents/Halite_project/Halite3_Python_MacOS/Halite3/replays/replay-20181028-124932-0400-1540745368-32-32.hlt',"bharddwaj")
--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 ./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 bots/testerBot.py" "python3 bots/MyBot.py"
+
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 bots/testMyBot.py" "python3 bots/testMyBot.py"
