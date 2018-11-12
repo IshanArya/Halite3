b0486dd427bef74ce95285a76e6bd81273092a27
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2018-11-12 13:33:14 -0500

    some minor changes

--- bots/bharddwaj_experiment_bot.py
+++ bots/bharddwaj_experiment_bot.py
@@ -17,6 +17,12 @@ while True:
     game.update_frame()
     me = game.me
     game_map = game.game_map
+    halite_amount_for_all_cells = {}
+    #list_of_information = []
+    for i in range(int(-width/2),int(width/2)+1): #when i used -width and width + 1 here i didn't get an error for some reason
+        for j in range(int(-height/2),int(height/2)+1):
+            some_position = Position(i,j)
+            halite_amount_for_all_cells[game_map[some_position].halite_amount] = some_position
     for ship in me.get_ships():
         logging.info("Looping among friendly ships has begun")
     if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
--- run_game.sh
+++ run_game.sh
@@ -16,3 +16,7 @@
 
 ./halite --replay-directory replays/ -vvv --no-logs --width 40 --height 40 "python3 bots/kingsmenv3Bot.py" "python3 bots/experimentOneBot.py"
 ./halite --replay-directory replays/ -vvv --no-logs --width 16 --height 16 "python3 bots/bharddwaj_experiment_bot.py" "python3 bots/experimentOneBot.py"
+
+#Bharddwaj's commands
+ python3 -m hlt_client play -b "/Users/bharddwajvemulapalli/Documents/Halite_project/Halite3_Python3_MacOS/Halite3/hlt" -r "python3 /Users/bharddwajvemulapalli/Documents/Halite_project/Halite3_Python3_MacOS/Halite3/bots/testMyBot.py" -r "python3 /Users/bharddwajvemulapalli/Documents/Halite_project/Halite3_Python3_MacOS/Halite3/bots/testMyBot.py" -i 10
+#getting permission denied from that for some reason
