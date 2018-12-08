c9977d409a81743253bce7eb527bddc2aa5cc123
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2018-12-08 10:48:26 -0500

    still have to fix the ships radius of information

--- bots/bharddwaj_experiment_bot.py
+++ bots/bharddwaj_experiment_bot.py
@@ -55,15 +55,12 @@ while True:
                 counter+=1
                 position_relative_to_ship_four = Position(-i,-j) + ship.position
                 counter+=1
-                list_of_ship_vision.append(position_relative_to_ship_one)
-                list_of_ship_vision.append(position_relative_to_ship_two)
-                list_of_ship_vision.append(position_relative_to_ship_three)
-                list_of_ship_vision.append(position_relative_to_ship_four)
-
-                iterations_for_below2 = 0
                 if not i == 0 and j == 0:
+                    list_of_ship_vision.append(position_relative_to_ship_one)
+                    list_of_ship_vision.append(position_relative_to_ship_two)
+                    list_of_ship_vision.append(position_relative_to_ship_three)
+                    list_of_ship_vision.append(position_relative_to_ship_four)
                     for i in halite_amount_based_dict:
-                        iterations_for_below2 += 1
                         temp_list = []
                         if position_relative_to_ship_one == halite_amount_based_dict[i][0]:
                             #temp_list.append(position_relative_to_ship_one) #Position
@@ -98,33 +95,32 @@ while True:
                         if not temp_list4 == []:
                             ship_specific_list.append(temp_list4)
                 else:
-                    for i in halite_amount_based_dict:
-                        some_list = []
-                        if ship.position == halite_amount_based_dict[i][0]:
-                            some_list.append((ship.position,halite_amount_based_dict[i][1]))
-                            if len(halite_amount_based_dict[i]) > 2:
-                                the_list =halite_amount_based_dict[i]
-                                some_list.append(the_list[2])
-                        if not some_list == []:
-                            ship_specific_list.append(some_list)
-
+                    list_of_ship_vision.append(ship.position)
+                    some_list = []
+                    some_list.append((ship.position,game_map[ship.position].halite_amount))
+                    ship_specific_list.append(some_list)
         #delete this loop later
         num_positions_in_dict = 0
+        not_in_dict = []
         for i in list_of_ship_vision:
             for j in halite_amount_based_dict:
                 if i == halite_amount_based_dict[j][0]:
                     num_positions_in_dict  += 1
+                    break
+                else:
+                    not_in_dict.append(halite_amount_based_dict[j][0])
+                    break
 
         logging.info(f'Length of Dictionary: {len(halite_amount_based_dict)}')
         logging.info(f"Width: {width}")
         logging.info(f"height: {height}")
         logging.info(f"iterations_for_below: {iterations_for_below} ")
-        logging.info(f"iterations_for_below: {iterations_for_below2} ")
         logging.info(f"Ship Specific List: {ship_specific_list}")
         logging.info(f"counter: {counter}")
         logging.info(f"Length of the list: {len(ship_specific_list)}")
         logging.info(f"Length of Ship Vision: {len(list_of_ship_vision)} ")
         logging.info(f"Num Positions: {num_positions_in_dict}")
+        logging.info(f"length of not in dict {len(not_in_dict)}")
     if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
         logging.info("Ship has just been spawned")
